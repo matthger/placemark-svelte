fbf0554b8915f9046ff201e8db95417bbf00a2d9
Matthias Gerhard <matthias.gerhard00@gmail.com>
2023-06-12 18:11:40 +0200

    about placemark

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import NavBar from "$lib/NavBar.svelte";
+    import PlacemarkInfoText from "$lib/PlacemarkInfoText.svelte";
+    import {goto} from "$app/navigation";
+    import {onMount} from "svelte";
+
+    onMount(() => {
+        if (localStorage.getItem("user")) goto("/user");
+    })
+</script>
+
+<svelte:head>
+    <title>Welcome to Placemark</title>
+</svelte:head>
+
+<NavBar />
+
+<PlacemarkInfoText context="main" />
